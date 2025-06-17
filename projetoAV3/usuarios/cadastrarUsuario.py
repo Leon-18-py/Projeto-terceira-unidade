2542ab00edab786b6b82ee7cfd684e7a648e03e6
Leon-18-py <leonrodobraga.cz@gmail.com>
2025-06-17 16:39:25 -0300

    Update cadastrarUsuario.py

--- projetoAV3/usuarios/cadastrarUsuario.py
+++ projetoAV3/usuarios/cadastrarUsuario.py
@@ -1,19 +1,21 @@
 def cadastrarUsuario (usuarios):
-    nome = input("Digite seu nome: ")
-    email = input("Digite seu email: ")
-    while(not "@" in email or not email.endswith('.com') and "gmail" in email):
-        print("Email inválido")
+    caminho = "C:\\Users\\COMPUTER\\Downloads\\Python_VS_code\\projetoAV3\\usuarios\\usuarios.txt"
+    with open(caminho, '+a', encoding='utf-8') as arquivo:
+        nome = input("Digite seu nome: ")
         email = input("Digite seu email: ")
-    senha = input("Digite sua senha: ")
-    usuario = {
+        while(not "@" in email or not email.endswith('.com') and "gmail" in email):
+            print("Email inválido")
+            email = input("Digite seu email: ")
+        senha = input("Digite sua senha: ")
+        usuario = {
         "nome": nome,
         "email": email,
         "senha": senha
-        }     
-    emailRepetido = email
-    encontrado = False
-    for usu in usuarios:
-        while(email == usu["email"]):
+          }     
+        emailRepetido = email
+        encontrado = False
+        for usu in usuarios:
+           while(email == usu["email"]):
             encontrado = True
             print("Email já existente. Tente outro: ")
             email = input("Digite seu email: ")
@@ -24,5 +26,10 @@ def cadastrarUsuario (usuarios):
                 "email": email,
                 "senha": senha
                     }    
-    usuarios.append(usuario)
-    print("Usuário cadastrado com sucesso!")
+        usuarios.append(usuario)
+        print("Usuário cadastrado com sucesso!")
+        for usu in usuario:
+            arquivo.write(f"Usuário: {nome} " + '\n')
+            arquivo.write(f"Email: {email}  " + '\n')
+            arquivo.write(f"Senha: {senha}  " + '\n')
+            break
